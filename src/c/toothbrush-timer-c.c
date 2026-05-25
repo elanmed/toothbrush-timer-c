3f6cf200ded66ed06ad438da222b92498b1ad6a6
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-05-25 00:08:14 -0400

    add: persist on close

--- src/c/toothbrush-timer-c.c
+++ src/c/toothbrush-timer-c.c
@@ -1,8 +1,5 @@
 #include <pebble.h>
 
-// TODO:
-// 1. Persist timer on close
-
 #define BRUSH_DURATION 120
 #define WAIT_DURATION 1800
 #define SIDEBAR_WIDTH 30
@@ -10,6 +7,8 @@
 #define AUTO_ADVANCE_DELAY_MS 2400
 #define REMAINING_FONT_SIZE 42
 #define DURATION_FONT_SIZE 20
+#define PERSIST_KEY_STATE 0
+#define PERSIST_KEY_WAKEUP_ID 1
 
 typedef enum { TIMER_STATE_PAUSED, TIMER_STATE_PLAYING } TimerState;
 
@@ -17,6 +16,7 @@ typedef struct {
   TimerState timer_state;
   int remaining_sec;
   int duration_sec;
+  time_t end_epoch;
 } AppState;
 
 static AppState s_state;
@@ -35,9 +35,12 @@ static AppTimer *s_play_start_delay_timer;
 static AppTimer *s_auto_advance_timer;
 static char s_remaining_text[8];
 static char s_duration_text[8];
+static WakeupId s_wakeup_id = -1;
+static bool s_timer_expired_while_closed = false;
 
 static void play_timer(void);
 static void toggle_timer(void);
+static void handle_timer_expired(bool should_vibrate);
 
 static void format_time(int sec, char *text_ptr, size_t text_size) {
   int minutes = sec / 60;
@@ -74,6 +77,23 @@ static void show_pause_icon(void) {
   bitmap_layer_set_bitmap(s_play_pause_icon_layer, s_pause_bitmap);
 }
 
+static void cancel_wakeup(void) {
+  if (s_wakeup_id >= 0) {
+    wakeup_cancel(s_wakeup_id);
+    s_wakeup_id = -1;
+  }
+  persist_delete(PERSIST_KEY_WAKEUP_ID);
+}
+
+static void schedule_wakeup(void) {
+  cancel_wakeup();
+  s_state.end_epoch = time(NULL) + s_state.remaining_sec;
+  s_wakeup_id = wakeup_schedule(s_state.end_epoch, 0, false);
+  if (s_wakeup_id >= 0) {
+    persist_write_int(PERSIST_KEY_WAKEUP_ID, s_wakeup_id);
+  }
+}
+
 static void auto_advance_after_brush_callback(void *context) {
   s_auto_advance_timer = NULL;
   int new_duration =
@@ -88,29 +108,17 @@ static void auto_advance_after_wait_callback(void *context) {
   toggle_timer();
 }
 
-static void on_second_tick(struct tm *tick_time, TimeUnits units_changed) {
-  s_state.remaining_sec -= 1;
-  sync_display();
-
-  if (s_state.duration_sec == BRUSH_DURATION) {
-    if (s_state.remaining_sec == 90 || s_state.remaining_sec == 60 ||
-        s_state.remaining_sec == 30) {
-      vibes_double_pulse();
-    }
+static void handle_timer_expired(bool should_vibrate) {
+  if (should_vibrate) {
+    static const uint32_t end_of_phase_segments[] = {200, 100, 200,  100, 200,
+                                                     100, 200, 1000, 200, 100,
+                                                     200, 100, 200,  100, 200};
+    static const VibePattern end_of_phase_pattern = {
+        .durations = end_of_phase_segments,
+        .num_segments = ARRAY_LENGTH(end_of_phase_segments),
+    };
+    vibes_enqueue_custom_pattern(end_of_phase_pattern);
   }
-
-  if (s_state.remaining_sec > 0) {
-    return;
-  }
-
-  static const uint32_t end_of_phase_segments[] = {200, 100, 200,  100, 200,
-                                                   100, 200, 1000, 200, 100,
-                                                   200, 100, 200,  100, 200};
-  static const VibePattern end_of_phase_pattern = {
-      .durations = end_of_phase_segments,
-      .num_segments = ARRAY_LENGTH(end_of_phase_segments),
-  };
-  vibes_enqueue_custom_pattern(end_of_phase_pattern);
 
   tick_timer_service_unsubscribe();
 
@@ -133,6 +141,25 @@ static void on_second_tick(struct tm *tick_time, TimeUnits units_changed) {
   }
 }
 
+static void on_second_tick(struct tm *tick_time, TimeUnits units_changed) {
+  s_state.remaining_sec -= 1;
+  sync_display();
+
+  if (s_state.duration_sec == BRUSH_DURATION) {
+    if (s_state.remaining_sec == 90 || s_state.remaining_sec == 60 ||
+        s_state.remaining_sec == 30) {
+      vibes_double_pulse();
+    }
+  }
+
+  if (s_state.remaining_sec > 0) {
+    return;
+  }
+
+  cancel_wakeup();
+  handle_timer_expired(true);
+}
+
 static void begin_second_tick_callback(void *context) {
   s_play_start_delay_timer = NULL;
   tick_timer_service_subscribe(SECOND_UNIT, on_second_tick);
@@ -151,6 +178,8 @@ static void play_timer(void) {
     app_timer_cancel(s_play_start_delay_timer);
   }
 
+  schedule_wakeup();
+
   uint16_t current_ms_into_second;
   time_ms(NULL, &current_ms_into_second);
   uint32_t ms_until_next_second = 1000 - current_ms_into_second;
@@ -159,6 +188,8 @@ static void play_timer(void) {
 }
 
 static void pause_timer(void) {
+  cancel_wakeup();
+
   if (s_play_start_delay_timer) {
     app_timer_cancel(s_play_start_delay_timer);
     s_play_start_delay_timer = NULL;
@@ -176,6 +207,8 @@ static void pause_timer(void) {
 }
 
 static void reset_timer(void) {
+  cancel_wakeup();
+
   s_state.remaining_sec = s_state.duration_sec;
   s_state.timer_state = TIMER_STATE_PAUSED;
   sync_display();
@@ -187,6 +220,8 @@ static void reset_timer(void) {
 }
 
 static void toggle_timer(void) {
+  cancel_wakeup();
+
   int new_duration =
       s_state.duration_sec == BRUSH_DURATION ? WAIT_DURATION : BRUSH_DURATION;
   s_state.duration_sec = new_duration;
@@ -238,6 +273,20 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, reset_click_handler);
 }
 
+static void wakeup_handler(WakeupId wakeup_id, int32_t cookie) {
+  s_wakeup_id = -1;
+  persist_delete(PERSIST_KEY_WAKEUP_ID);
+
+  if (s_state.timer_state == TIMER_STATE_PLAYING ||
+      s_auto_advance_timer != NULL) {
+    return;
+  }
+
+  s_state.remaining_sec = 0;
+  sync_display();
+  handle_timer_expired(true);
+}
+
 static void sidebar_layer_update_proc(Layer *layer, GContext *ctx) {
   GRect sidebar_bounds = layer_get_bounds(layer);
   graphics_context_set_fill_color(ctx, GColorBlack);
@@ -272,7 +321,7 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_remaining_label, GTextAlignmentCenter);
   layer_add_child(root_layer, text_layer_get_layer(s_remaining_label));
 
-  int duration_label_padding = 5;
+  int duration_label_padding = 10;
   GRect duration_label_rect =
       GRect(duration_label_padding,
             root_bounds.size.h - DURATION_FONT_SIZE - duration_label_padding,
@@ -298,12 +347,28 @@ static void main_window_load(Window *window) {
       0, sidebar_bounds, gbitmap_get_bounds(s_toggle_bitmap).size));
   bitmap_layer_set_bitmap(s_toggle_icon_layer, s_toggle_bitmap);
   bitmap_layer_set_compositing_mode(s_toggle_icon_layer, GCompOpSet);
+  bool show_toggle_icon = s_state.timer_state == TIMER_STATE_PAUSED &&
+                          s_state.duration_sec == s_state.remaining_sec;
+  if (!show_toggle_icon) {
+    layer_set_hidden(bitmap_layer_get_layer(s_toggle_icon_layer), true);
+  }
   layer_add_child(s_sidebar_layer, bitmap_layer_get_layer(s_toggle_icon_layer));
 
+  GBitmap *play_pause_bitmap;
+  if (s_state.timer_state == TIMER_STATE_PAUSED) {
+    play_pause_bitmap = s_play_bitmap;
+  } else {
+    play_pause_bitmap = s_pause_bitmap;
+  }
+
   s_play_pause_icon_layer = bitmap_layer_create(centered_icon_rect_in_slot(
-      1, sidebar_bounds, gbitmap_get_bounds(s_play_bitmap).size));
-  bitmap_layer_set_bitmap(s_play_pause_icon_layer, s_play_bitmap);
+      1, sidebar_bounds, gbitmap_get_bounds(play_pause_bitmap).size));
+  bitmap_layer_set_bitmap(s_play_pause_icon_layer, play_pause_bitmap);
   bitmap_layer_set_compositing_mode(s_play_pause_icon_layer, GCompOpSet);
+  bool show_play_pause_icon = s_auto_advance_timer == NULL;
+  if (!show_play_pause_icon) {
+    layer_set_hidden(bitmap_layer_get_layer(s_play_pause_icon_layer), true);
+  }
   layer_add_child(s_sidebar_layer,
                   bitmap_layer_get_layer(s_play_pause_icon_layer));
 
@@ -311,7 +376,11 @@ static void main_window_load(Window *window) {
       2, sidebar_bounds, gbitmap_get_bounds(s_reset_bitmap).size));
   bitmap_layer_set_bitmap(s_reset_icon_layer, s_reset_bitmap);
   bitmap_layer_set_compositing_mode(s_reset_icon_layer, GCompOpSet);
-  layer_set_hidden(bitmap_layer_get_layer(s_reset_icon_layer), true);
+  bool show_reset_icon = s_state.timer_state == TIMER_STATE_PAUSED &&
+                         s_state.duration_sec != s_state.remaining_sec;
+  if (!show_reset_icon) {
+    layer_set_hidden(bitmap_layer_get_layer(s_reset_icon_layer), true);
+  }
   layer_add_child(s_sidebar_layer, bitmap_layer_get_layer(s_reset_icon_layer));
 
   window_set_click_config_provider(window, click_config_provider);
@@ -327,12 +396,50 @@ static void main_window_unload(Window *window) {
   layer_destroy(s_sidebar_layer);
 }
 
+static void save_state(void) {
+  persist_write_data(PERSIST_KEY_STATE, &s_state, sizeof(AppState));
+}
+
+static void load_state(void) {
+  if (!persist_exists(PERSIST_KEY_STATE)) {
+    return;
+  }
+
+  AppState saved_state;
+  persist_read_data(PERSIST_KEY_STATE, &saved_state, sizeof(AppState));
+
+  if (persist_exists(PERSIST_KEY_WAKEUP_ID)) {
+    s_wakeup_id = persist_read_int(PERSIST_KEY_WAKEUP_ID);
+  }
+
+  if (saved_state.timer_state == TIMER_STATE_PLAYING) {
+    int recalculated_remaining = (int)(saved_state.end_epoch - time(NULL));
+    if (recalculated_remaining > 0) {
+      s_state = saved_state;
+      s_state.remaining_sec = recalculated_remaining;
+    } else {
+      s_state.duration_sec = saved_state.duration_sec;
+      s_state.timer_state = TIMER_STATE_PAUSED;
+      s_state.remaining_sec = 0;
+      s_timer_expired_while_closed = true;
+    }
+  } else if (saved_state.timer_state == TIMER_STATE_PAUSED &&
+             saved_state.remaining_sec == 0) {
+    s_state = saved_state;
+    s_timer_expired_while_closed = true;
+  } else {
+    s_state = saved_state;
+  }
+}
+
 static void init(void) {
   s_state = (AppState){
       .timer_state = TIMER_STATE_PAUSED,
       .remaining_sec = BRUSH_DURATION,
       .duration_sec = BRUSH_DURATION,
   };
+
+  load_state();
 
   s_play_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PLAY);
   s_pause_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PAUSE);
@@ -345,13 +452,23 @@ static void init(void) {
                                                 .unload = main_window_unload,
                                             });
   window_stack_push(s_main_window, true);
+
+  wakeup_service_subscribe(wakeup_handler);
+
+  if (s_state.timer_state == TIMER_STATE_PLAYING) {
+    play_timer();
+  }
 }
 
 static void deinit(void) {
-  if (s_play_start_delay_timer)
+  save_state();
+
+  if (s_play_start_delay_timer) {
     app_timer_cancel(s_play_start_delay_timer);
-  if (s_auto_advance_timer)
+  }
+  if (s_auto_advance_timer) {
     app_timer_cancel(s_auto_advance_timer);
+  }
   tick_timer_service_unsubscribe();
 
   gbitmap_destroy(s_play_bitmap);
